6d83c55736916e433a6e3c756655da4be1cf6d7f
niedzwieckimn-ctrl <niedzwiecki.mn@gmail.com>
2025-09-03 22:10:48 +0200

    Add files via upload

---
+++ lib/prisma.ts
@@ -0,0 +1,13 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // ¿eby unikn¹æ wielu instancji w trybie dev
+  var prisma: PrismaClient | undefined
+}
+
+const client = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') global.prisma = client
+
+export default client
